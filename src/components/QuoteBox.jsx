99d742e8b00b3388e720cbf97690b2d7e6605d5e
Miguel Martinez <mike280715@gmail.com>
2023-08-01 13:01:48 -0600

    se colocó por quien fue creado

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -13,11 +13,12 @@ const QuoteBox = ({handleChangeQuote, quote}) => {
           <p className="phrase">
            {quote.phrase}
           </p>
+          <footer className="footer_card"><h4>Fuente: {quote.author}</h4></footer>
         </article>
         <button className="button" onClick={handleChangeQuote}>Try luck</button>
       </section>
 
-      <footer className="footer"><h4 className="footer_font">Fuente: {quote.author}</h4></footer>
+      <footer className="footer"><h4>created by: Miguel Martinez &copy; 2023 </h4></footer>
     </section>
   );
 };
